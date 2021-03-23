3f01e9a1eab8b5540763c436c4ed688942da5550
Dipika29 <dipibhanushali@gmail.com>
2021-03-23 10:27:34 +0530

    edited cube function

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -20,8 +20,8 @@ public class Calculator {
 	public static void square(int n1){
 		System.out.println(n1*n1);
 	}
-	public static void pow(int n1){
-		System.out.println(n1*n1);
+	public static void cube(int n1){
+		System.out.println(n1*n1*n1);
 	}
 
 }
